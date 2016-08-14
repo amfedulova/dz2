7c431865e7e3fbea9f0854310dcd462455faa829
amfedulova <amfedulova@gmail.com>
2016-08-14 22:14:41 +0300

    first commit

---
+++ homework2.php
@@ -0,0 +1,14 @@
+<?php
+echo "<h4>Задание 1<h4/>";
+function f1($str=array(caffe,tea,juce))
+{
+    foreach ($str as $value)
+    echo "$value</p>";
+}
+f1();
+/**
+ * Created by PhpStorm.
+ * User: Анна
+ * Date: 14.08.2016
+ * Time: 21:07
+ */
